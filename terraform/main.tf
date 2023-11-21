284ac9029bae05eb5ac981f5fe1fb522638ab1dd
HoussemRhim <houssem.hrh@gmail.com>
2023-11-21 13:08:51 +0100

    create bucket for processed data in GCP

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,13 @@ resource "google_storage_bucket" "my_gcs_bucket" {
   location = "europe-west3"
   #force_destroy = true
 }
+
+resource "google_storage_bucket" "my_gcs_bucket2" {
+  name     = "data_bucket_processed"
+  location = "europe-west3"
+  #force_destroy = true
+}
+
 variable "gcp_services_account_key" {
   description = "The GCP service account key"
   default = ""
